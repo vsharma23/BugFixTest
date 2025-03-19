67f377d9b8c76651b780f1073cf71915d36f87aa
vsharma23 <85680350+vsharma23@users.noreply.github.com>
2025-03-19 16:07:05 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -2,10 +2,10 @@ def binary_search(arr, target):
     low, high = 0, len(arr) - 1
     while low <= high:
         mid = (low + high) // 2
-        if arr[mid] < target:
-            low = mid + 1
+        if arr[mid] > target:
+            low = mid - 1
         elif arr[mid] > target:
-            high = mid - 1
+            high = mid + 1
         else:
             return mid
     return -1
